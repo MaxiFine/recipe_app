88a173febe529a2af80bb461db981a357c584cfc
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-02-13 15:44:38 +0000

    adding recipe deatails page

---
+++ lib/recipe_detail.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'recipe.dart';
+
+class RecipeDetail extends StatefulWidget {
+  final Recipe recipe;
+  const RecipeDetail({Key? key, required this.recipe, 
+  }) : super(key: key);
+  @override
+  _RecipeDetailState createState() {
+    return _RecipeDetailState();
+  }
+}
+
+class _RecipeDetailState extends State<RecipeDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.recipe.label),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+             SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image(
+                image: AssetImage(widget.recipe.imageUrl),
+                fit: BoxFit.cover,
+              ),
+              ),
+              const SizedBox(
+                height: 4.0,
+              ),
+              Text(
+                widget.recipe.label,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+              ),
+              
+          ],
+        ),
+      ),
+    );
+  }
+}
